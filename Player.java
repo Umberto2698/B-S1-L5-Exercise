8f6772a6c7dacf76266b074363bf6cabe4b2861d
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-10-06 10:49:53 +0200

    Added Video class

--- Player.java
+++ Player.java
@@ -1,9 +1,11 @@
 import mediaPlayer.RegistrazioneAudio;
+import mediaPlayer.Video;
 
 public class Player {
     public static void main(String[] args) {
         RegistrazioneAudio registrazione = new RegistrazioneAudio("prova", 3);
-
+        Video video = new Video("video prova", 4);
         registrazione.play();
+        video.play();
     }
 }
---
+++ mediaPlayer/Video.java
@@ -0,0 +1,64 @@
+package mediaPlayer;
+
+public class Video extends ElementoMultimediale implements RegolazioneVolume, RegolazioneLuminosità, Riproducibili {
+    int volume = 5;
+    int durata;
+    int luminosità = 3;
+
+
+    public Video(String titolo, int durata) {
+        this.titolo = titolo;
+        this.durata = durata;
+    }
+
+    @Override
+    public void alzaVolume() {
+        if (this.volume < 10) {
+            this.volume++;
+        } else {
+            System.out.println("Il volume è già al massimo.");
+        }
+    }
+
+    @Override
+    public void abbassaVolume() {
+        if (this.volume > 0) {
+            this.volume--;
+        } else {
+            System.out.println("Il volume è già al minimo.");
+        }
+    }
+
+    @Override
+    public void alzaLuminosità() {
+        if (this.luminosità < 10) {
+            this.luminosità++;
+        } else {
+            System.out.println("La luminosità è già al massimo.");
+        }
+    }
+
+    @Override
+    public void abbassaLuminosità() {
+        if (this.luminosità > 0) {
+            this.luminosità--;
+        } else {
+            System.out.println("La luminosità è già al minimo.");
+        }
+    }
+
+
+    @Override
+    public void play() {
+        String str = "";
+        for (int i = 0; i < this.volume; i++) {
+            str += "!";
+        }
+        for (int i = 0; i < this.luminosità; i++) {
+            str += "*";
+        }
+        for (int i = 0; i < this.durata; i++) {
+            System.out.println(this.titolo + str);
+        }
+    }
+}
